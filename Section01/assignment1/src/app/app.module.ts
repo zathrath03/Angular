1da565178e9057d3cbca413c872ca33da967bee1
John Dugger <zathrath03@gmail.com>
2023-02-19 17:25:02 -0800

    Add a red Warning Alert Component

--- Section01/assignment1/src/app/app.module.ts
+++ Section01/assignment1/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warningAlert/warningAlert.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        WarningAlertComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule
+    ]
 })
 export class AppModule { }
---
+++ Section01/assignment1/src/app/warningAlert/warningAlert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'warning-alert',
+  templateUrl: './warningAlert.component.html',
+  styleUrls: ['./warningAlert.component.css']
+})
+export class WarningAlertComponent {}
