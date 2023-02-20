21cc87e8d9cd651b2693ec44b75c15129dc10cef
John Dugger <zathrath03@gmail.com>
2023-02-19 17:29:50 -0800

    Update warningAlert.component to use the correct naming scheme

--- Section01/assignment1/src/app/app.module.ts
+++ Section01/assignment1/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warningAlert/warningAlert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
--- Section01/assignment1/src/app/warning-alert/warning-alert.component.ts
+++ Section01/assignment1/src/app/warning-alert/warning-alert.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'warning-alert',
-  templateUrl: './warningAlert.component.html',
-  styleUrls: ['./warningAlert.component.css']
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.css']
 })
 export class WarningAlertComponent {}
