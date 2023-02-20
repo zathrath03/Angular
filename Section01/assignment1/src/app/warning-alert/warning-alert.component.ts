6ce7f25e040de1ce2449a8f726f40f023067c680
John Dugger <zathrath03@gmail.com>
2023-02-19 17:33:56 -0800

    Correct name of warning-alert to app-warning-alert

--- Section01/assignment1/src/app/warning-alert/warning-alert.component.ts
+++ Section01/assignment1/src/app/warning-alert/warning-alert.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: 'warning-alert',
+  selector: 'app-warning-alert',
   templateUrl: './warning-alert.component.html',
   styleUrls: ['./warning-alert.component.css']
 })
