f0e23499928684bb5c80cba4a9b1c9a4d1da6dfa
John Dugger <zathrath03@gmail.com>
2023-02-22 12:12:58 -0800

    Passing data using event bindings

--- Section01/my-first-app/src/app/servers/servers.component.ts
+++ Section01/my-first-app/src/app/servers/servers.component.ts
@@ -10,8 +10,18 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() {
     setTimeout(() => this.allowNewServer = true, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
